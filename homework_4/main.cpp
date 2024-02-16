a5622fcded284944e38323ffb8de1b0221a67fa6
Leison Gao <leisongao2005@g.ucla.edu>
2024-02-16 15:34:28 -0800

    Initial Commit

---
+++ homework_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework_4
+//
+//  Created by Leison Gao on 2/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
